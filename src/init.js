522bae495f5409c1bca257d40525ced75b3161b0
cwm9cwm9 <github.reg@chiem.ma>
2021-03-16 19:53:59 -1000

    This is a minor rewrite with some new functionality being offered for consideration.
    Changes:
    
    1. Using a remote service to tunnel is not necessary, as the URL given as a forwarding address in the browser.  Removed the external service (which I feel uncomfortable routing a major financial key through) and replaced it with a local HTTPS server.
    2. Calling init now only starts the HTTPS server.
    3. Calling token will automatically launch the browser if no token is currently available, or if the existing token is stale.  The program will wait until the user logs in, then get the key and continue.
    
    NOTES:
    You MUST provide a .crt and .key file for the HTTPS server to run properly.  Generate one with the following commands:
    
    Install openssl (or equivalent) if you do not already an sn sll implementation
    
    In the program directory:
    
    openssl genrsa 2048 > server.key
    chmod 400 host.key (not needed on windows)
    openssl req -new -x509 -nodes -sha256 -key server.key -out server.crt
    
    Signed-off-by: cwm9cwm9 <github.reg@chiem.ma>

--- src/init.js
+++ src/init.js
@@ -1,11 +1,42 @@
-const { promises: fs } = require('fs');
+var fs = require('fs');
+const { promises: pfs } = require('fs');
+var https = require('https');
+var privateKey = fs.readFileSync('server.key', 'utf8');
+var certificate = fs.readFileSync('server.crt', 'utf8');
+var credentials = { key: privateKey, cert: certificate };
 const ngrok = require('ngrok');
 const express = require('express');
 const rp = require('request-promise');
 const moment = require('moment');
 const { secretsfile } = require('./constants');
+const Open = require('open');
 
+// Choose the port you would like to use for your server
 const port = 2828;
+const callbackUrl = `https://localhost:` + port;
+
+const eventsSharedBuffer = new SharedArrayBuffer(4); // we only a single event.  Automatically initialized to 0 by constructor.
+const events = new Int32Array(eventsSharedBuffer);
+
+var clientId;
+
+async function login(appkeyparam) {
+    console.log("No token available.  Requesting one from user.")
+
+    await Open(`https://auth.tdameritrade.com/auth?response_type=code&redirect_uri=${encodeURI(callbackUrl)}&client_id=${encodeURI(clientId)}`)
+
+    console.log(`
+Opening browser for login and awaiting connection from TD Ameritrade...
+
+If you get an error in your browers, you have not yet created an app in your TD Ameritrade Developer account:
+
+Login to your developer account here: https://developer.tdameritrade.com/
+            Then, create an app here: https://developer.tdameritrade.com/user/me/apps
+          ...using this callback URL: ${callbackUrl}
+        `);
+    // Don't block the message thread --- keep 
+    while (Atomics.load(events, 0) == 0) await new Promise(resolve => setTimeout(resolve, 100));
+}
 
 async function init(appkeyparam) {
   const appkey = appkeyparam || process.env.TDAUTH_APPKEY;
@@ -13,15 +44,10 @@ async function init(appkeyparam) {
     throw new Error('app key must be passed into init function or set as TDAUTH_APPKEY environment variable');
   }
 
-  const clientId = `${appkey}@AMER.OAUTHAP`;
-
-  const url = await ngrok.connect({
-    addr: port,
-  });
-  const callbackUrl = `${url}/cb`;
+    clientId = `${appkey}@AMER.OAUTHAP`;
 
   const app = express();
-  app.get('/cb', async (req, res, next) => {
+  app.get('/', async (req, res, next) => {
     try {
       const { code } = req.query;
       const options = {
@@ -39,13 +65,11 @@ async function init(appkeyparam) {
       };
       const data = await rp(options);
       data.now = moment().unix();
-      await fs.writeFile(secretsfile, JSON.stringify(data, null, 2));
-      res.json({ ok: 'ok', data });
-      console.log(`
-        Thank you for initializing td-ameritrade-auth.
-        The library is ready to use, and getToken should now return an up to date access_token as needed.
-      `);
-      return process.exit(0);
+            await pfs.writeFile(secretsfile, JSON.stringify(data, null, 2));
+            res.send('Your key has been recorded and you may now close this window.');
+            console.log("notifying");
+            Atomics.store(events, 0, 1);
+            Atomics.notify(events, 0, 1);
     } catch (err) {
       return next(err);
     }
@@ -58,25 +82,15 @@ async function init(appkeyparam) {
   });
 
   return new Promise((resolve, reject) => {
-    app.listen(port, (err) => {
-      if (err) {
-        reject(err);
-        return;
-      }
-
-      console.log(`
-        Navigate to your app settings in TD Ameritrade and set the callback url to ${callbackUrl}.
-
-
-        Apps:           https://developer.tdameritrade.com/user/me/apps
-        Callback URL:   ${callbackUrl}
-       
-
-        Once the app has been configured, navigate to the following address to authenticate your application:
-        https://auth.tdameritrade.com/auth?response_type=code&redirect_uri=${encodeURI(callbackUrl)}&client_id=${encodeURI(clientId)}
-      `);
+        httpsServer=https.createServer(credentials, app);
+        httpsServer.listen(port, (err) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+        });
+        resolve();
     });
-  });
 }
 
-module.exports = init;
+module.exports = { init, login };
